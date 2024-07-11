8760463d105f11fb0d17885ef33b5e35e33f9316
DylanapH <dapharri@icloud.com>
2024-07-11 17:11:39 +0100

    funtion time

--- main.c
+++ main.c
@@ -1,49 +1,42 @@
 #include <iso646.h>
 #include <stdio.h>
 
-int main(void)
-{
-    int a = 1;
-    int b = 1;
-    int i=0;
-    float w = 0;
-    float l = 0;
-    float t= 0;
-    int totalside = 25;
-    float results[totalside];
+void rollies(int dice1, int dice2) { //this is a function with the paramiters being the dice sides
+    int a = 1; // initialised values, dice 1
+    int b = 1; // dice 2
+    int i=0; // array position
+    float w = 0; //win values
+    float l = 0; // loss values
+    float t= 0; // tie values
+    int totalside = dice1* dice2; //total sides
+    float results[totalside]; // array of results, max ammount of sides is the max ammount of values
     while (i<totalside) {
-        while (a<=5) { // 6 = number of sides on the dice
-            while (b<=5) // 6 = number of sides on the dice
+        while (a<=dice1) { // 6 = number of sides on the dice
+            while (b<=dice2) // 6 = number of sides on the dice
             {
                 results[i] = a - b;
                 printf("%f\n", results[i]);
                 if (results[i]>0){printf("win \n ");++w;}
-                if (results[i]<0){printf("loss \n ");++l;}
-                if (results[i]==0){printf("tie \n ");++t;}
+                if (results[i]<0){printf("loss \n ");++l;} //printing function for the percentadge
+                if (results[i]==0){printf("tie \n ");++t;} // also incrimenting win,loss,tie values
                 ++i;
-                ++b;
+                ++b; // incrimenting b to use the next value
 
 
             }
-            b=1;
+            b=1; // resetting b to restart the process with a different a value
             ++a;
         }
         a=1;
-        printf("%f\n", l/totalside);
+        printf("%f\n", l/totalside); // printing the outcome percentages
         printf("%f\n", t/totalside);
         printf("%f\n", w/totalside);
-
     }
-    /*
-    printf("%f\n", w/totalside);
-    printf("%f\n", l/totalside);
-    printf("%f\n", t/totalside);
-*/
-
-
 
 }
-
+int main(void) {// this is the actual program being ran, it uses the prev function
+   rollies(5,5);// dice sides
+}
 
 //print count num of things more than 0 as "numberr of wins equlas insert here"
 /*int i = 0;
